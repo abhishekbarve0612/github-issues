d97fae274d9775d79bd233d5ccffec6487153859
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-04-13 15:54:20 +0530

    Completed Github issues assignment

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Issues from "./components/Issues";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <header className="App-header">
+          <Link to="/">GIT HUB Issues</Link>
+        </header>
+        <Switch>
+          <Route exact path="/:page?">
+            <Issues />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
